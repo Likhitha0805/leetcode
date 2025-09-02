2623798d74a1ef62b0a5f75e70fe448262f35fb3
Likhitha0805 <likhithakokku85@gmail.com>
2025-09-02 22:52:26 +0530

    Time: 4 ms (12.78%), Space: 41.7 MB (84.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
+                stack.push(s.charAt(i));
+            }
+          
+          else{
+            if(!stack.empty()&&
+            ((stack.peek()=='('&&s.charAt(i)==')')||(stack.peek()=='{'&&s.charAt(i)=='}')||(stack.peek()=='['&&s.charAt(i)==']'))){
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+
+        
+    
+
+          }
+        }
+      
+    
+return stack.empty();
+}
+}
