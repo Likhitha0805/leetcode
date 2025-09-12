35912978a70cf2e41fdf2b50662ea993d0f5d54e
Likhitha0805 <likhithakokku85@gmail.com>
2025-09-12 22:20:59 +0530

    Time: 3 ms (91.34%), Space: 44.7 MB (43.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        else{
+            char[]charArray1=s.toCharArray();
+            char[]charArray2=t.toCharArray();
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+            if(Arrays.equals(charArray1,charArray2))
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+                
+
+
+            }
+
+        }
+    
